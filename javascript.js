4be3425e7181388de313e5f77cc8294103a92a57
harcengaming <65104722+harcengaming@users.noreply.github.com>
2021-07-29 13:46:55 +0700

    tambahan gambar buah

--- javascript.js
+++ javascript.js
@@ -52,40 +52,44 @@ function printAll() {
   };
 };
 
-// printAll();
-console.log(printAll());
-
-
 function printByID(id) {
-  return todos[id-1].todo;
+  return todos[id - 0].todo;
 };
-
-console.log("Print by ID:", printByID(1));
-
 
 function add(newTodo) {
   todos.push({
-    id: todos.length - 1,
-    todo: "ngerjain tugas",
+    id: todos.length + 1,
+    todo: newTodo,
   });
-  return newTodo;
+  return todos;
 };
-
-
-// const newToDo = todos.push({
-//     id: todos.length - 1,
-//     todo: "ngerjain tugas",
-// });
-// console.log(newToDo);
 
 function deleteByID(id) {
-  return todos[id-1].todo
+  todos.splice(id - 1, 1);
+  return todos;
 };
 
+// function updateByID(id) {
+//   todos.splice[id + 2].todo;
+//   return todos;
+// };
 
-function updateBYID(newTodo) {
-  return todos[id+2].todo
-};
+objIndex = todos.findIndex((obj => obj.id == 1));
+
+//Log object to Console.
+console.log("Before update: ", todos[objIndex]);
+
+//Update object's name property.
+todos[objIndex].todo = "belajar CRUD"
+
+
+printAll();
+console.log("Print by ID:", printByID(1));
+console.log("Add : ", add("ngerjain tugas"));
+console.log("Delete : ", deleteByID(2));
+// console.log("Update :", updateByID(2));
+console.log("Update: ", todos[objIndex]);
+
 
 //No. 04
 let buahBuah = 
@@ -93,53 +97,77 @@ let buahBuah =
   {
     nama: 'Apple',
     warna: 'Merah',
-    namaLatin: 'Molus sylvestris',
-    tipeBiji: 'single seed',
+    nama_latin: 'Molus sylvestris',
+    tipe_biji: 'single seed',
+    gambar_buah: 'images/apple.png',
   },
   {
     nama: 'Banana',
     warna: 'Kuning',
-    namaLatin: 'Musa paradisiaca',
-    tipeBiji: 'double seed',
+    nama_latin: 'Musa paradisiaca',
+    tipe_biji: 'double seed',
+    gambar_buah: 'images/banane-large.jpg',
   },
   {
     nama: 'Blueberry',
     warna: 'Biru tua keunguan',
-    namaLatin: 'Vaccinium Carymbosum',
-    tipeBiji: 'single seed',
+    nama_latin: 'Vaccinium Carymbosum',
+    tipe_biji: 'single seed',
+    gambar_buah: 'images/blueberry.jpg',
   },
   {
     nama: 'Cherry',
     warna: 'Merah cerah',
-    namaLatin: 'Prunus Apetala',
-    tipeBiji: 'double seed',
+    nama_latin: 'Prunus Apetala',
+    tipe_biji: 'double seed',
+    gambar_buah: 'images/cherry.jpg',
   },
   {
     nama: 'Lemon',
     warna: 'Kuning',
-    namaLatin: 'Citrus Limon',
-    tipeBiji: 'single seed',   
+    nama_latin: 'Citrus Limon',
+    tipe_biji: 'single seed',
+    gambar_buah: 'images/lemon.png',   
   },
   {
     nama: 'Mango',
     warna: 'Kuning serta Jingga',
-    namaLatin: 'Citrus Reticulata',
-    tipeBiji: 'double seed',
+    nama_latin: 'Citrus Reticulata',
+    tipe_biji: 'double seed',
+    gambar_buah: 'images/mango.jpg',
   },
   {
     nama: 'Orange',
     warna: 'Geoluhread atau kuning-merah',
-    namaLatin: 'Molus sylvestris',
-    tipeBiji: 'single seed',
+    nama_latin: 'Molus sylvestris',
+    tipe_biji: 'single seed',
+    gambar_buah: 'images/orange.jpg',
   },
   {
     nama: 'Pear',
     warna: 'Kuning keemasan ataupun hijau',
-    namaLatin: 'Pyrus Caucasica',
-    tipeBiji: 'double seed',
+    nama_latin: 'Pyrus Caucasica',
+    tipe_biji: 'double seed',
+    gambar_buah: 'images/pear.jpg',
   }
 ];
 
 buahBuah.forEach((listbuah) => {
   console.log(listbuah);
 });
+
+
+
+for (let i = 0; i < 8; i++) {
+  document.write(
+    `<div id="buah">
+      <img src=${buahBuah[i].gambar_buah} />  
+      <p>${buahBuah[i].nama}</p>
+      <p>${buahBuah[i].warna}</p>
+      <p>${buahBuah[i].nama_latin}</p>
+      <p>${buahBuah[i].tipe_biji}</p>
+      
+    </div>`);
+};
+
+//selesai
